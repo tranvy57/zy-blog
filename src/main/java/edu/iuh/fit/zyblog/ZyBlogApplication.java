56f7887af9171cdd03b3157673c52f03fcc270f8
tranvy57 <tranvy.art@gmail.com>
2024-11-09 18:57:54 +0700

    init project

---
+++ src/main/java/edu/iuh/fit/zyblog/ZyBlogApplication.java
@@ -0,0 +1,13 @@
+package edu.iuh.fit.zyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZyBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/iuh/fit/zyblog/ZyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iuh.fit.zyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
